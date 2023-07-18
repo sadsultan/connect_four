56cbb74cdbad1f673214e54763ed31604f1e5f2c
Saad <66124515+sadsultan@users.noreply.github.com>
2023-07-19 04:54:08 +0500

    Add ability to place token

--- script.js
+++ script.js
@@ -7,3 +7,13 @@ for (let i = 0; i < columns; i++){
         board[i][j]=0;
     }
 }
+
+function placeToken(selection, playerToken) {
+    const column=board[selection];
+    const value = column.findIndex( (element) => element === 0);
+    if (value === -1) return false;
+    else {
+        column[value]=playerToken;
+        return true;
+    }
+}
