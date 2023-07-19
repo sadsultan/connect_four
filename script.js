61e3ad9317e14e7d21fd191f044454d8703d4d9c
Saad <66124515+sadsultan@users.noreply.github.com>
2023-07-19 06:19:18 +0500

    Add setBoard function

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
 const rows= 6;
 const columns=7;
 let board=[];
-for (let i = 0; i < columns; i++){
-    board[i]=[];
-    for (let j = 0; j < rows; j++){
-        board[i][j]=0;
+
+function setBoard(){
+    for (let i = 0; i < columns; i++){
+        for (let j = 0; j < rows; j++){
+            board[i][j]=0;
+        }
     }
 }
 
