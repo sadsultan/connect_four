c85daec9f6d6b3a16e8a61685c9779398f718f62
Saad <66124515+sadsultan@users.noreply.github.com>
2023-07-19 21:10:41 +0500

    Remove global board

--- script.js
+++ script.js
@@ -3,24 +3,25 @@ const columns=7;
 let board=[];
 
 function setBoard(){
+    let board=[];
     for (let i = 0; i < columns; i++){
         for (let j = 0; j < rows; j++){
             board[i][j]=0;
         }
     }
+    return board;
 }
 
-function placeToken(selection, playerToken) {
-    let column = board[selection];
-    let value = column.findIndex( (element) => element === 0);
+function placeToken(board, selection, playerToken){
+    let value = board[selection].findIndex( (element) => element === 0);
     if (value === -1) return false;
     else {
-        column[value]=playerToken;
-        return [selection, value];
+        board[selection][value]=playerToken;
+        return [board ,[selection, value]];
     }
 }
 
-function checkWin(latestAddition){
+function checkWin(board ,latestAddition){
     let i = latestAddition[0];
     let j = latestAddition[1];
 
@@ -31,13 +32,13 @@ function checkWin(latestAddition){
 
     for (; i < columns-3 && j < rows-3; i++,j++){
         // Testing the diagonals
-        if (board[i][j] && board[i+1][j+1] && board[i+2][j+2] && board[i+3][j+3]) return true;
+        if (board[i][j] && board[i+1][j+1] && board[i+2][j+2] && board[i+3][j+3]) return board[i][j];
         // Testing the columns 
-        if (board[latestAddition[0]][j] && board[latestAddition[0]][j+1] && board[latestAddition[0]][j+2] && board[latestAddition[0]][j+3]) return true;
+        if (board[latestAddition[0]][j] && board[latestAddition[0]][j+1] && board[latestAddition[0]][j+2] && board[latestAddition[0]][j+3]) return board[latestAddition[0]][j];
         // Testing the rows
-        if (board[i][latestAddition[1]] && board[i+1][latestAddition[1]] && board[i+2][latestAddition[1]] && board[i+3][latestAddition[1]]) return true;
+        if (board[i][latestAddition[1]] && board[i+1][latestAddition[1]] && board[i+2][latestAddition[1]] && board[i+3][latestAddition[1]]) return board[i][latestAddition[1]];
     }
-    return false;
+    return 0;
 }  
 
 function checkDraw(){
@@ -45,4 +46,14 @@ function checkDraw(){
         if (board[i].includes(0)) return false;
     }
     return true;
+}
+
+function computerMove() {
+    // Use a minmax algorithm using alpha-beta pruning to determine the best move here
+
+}
+
+function evaluateBoard(){
+    // Evaluate the board here
+    
 }
