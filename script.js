3a0d5ca25df3e2c492dd6918fb08d12a8bf27f48
Saad <66124515+sadsultan@users.noreply.github.com>
2023-07-19 06:05:34 +0500

    Added checkWin function

--- script.js
+++ script.js
@@ -9,11 +9,30 @@ for (let i = 0; i < columns; i++){
 }
 
 function placeToken(selection, playerToken) {
-    const column=board[selection];
-    const value = column.findIndex( (element) => element === 0);
+    let column = board[selection];
+    let value = column.findIndex( (element) => element === 0);
     if (value === -1) return false;
     else {
         column[value]=playerToken;
-        return true;
+        return [selection, value];
     }
 }
+
+function checkWin(latestAddition){
+    let i = latestAddition[0];
+    let j = latestAddition[1];
+    for (let k=0; k < 3; k++){
+        if(i && j){
+            i--;
+            j--;
+        }else{
+            break;
+        } 
+    }
+    for (; i < columns-3 && j < rows-3; i++,j++){
+        if (board[i][j] && board[i+1][j+1] && board[i+2][j+2] && board[i+3][j+3]) return true;
+        if (board[latestAddition[0]][j] && board[latestAddition[0]][j+1] && board[latestAddition[0]][j+2] && board[latestAddition[0]][j+3]) return true;
+        if (board[i][latestAddition[1]] && board[i+1][latestAddition[1]] && board[i+2][latestAddition[1]] && board[i+3][latestAddition[1]]) return true;
+    }
+    return false;
+}  
